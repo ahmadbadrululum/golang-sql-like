13d87d997c98da47bc9e4b91d35962ad74e6fc53
Badrules <ahmadbadrul635@gmail.com>
2023-02-22 23:40:12 +0700

    sample json tag

--- entities/product_entity.go
+++ entities/product_entity.go
@@ -1,14 +1,23 @@
 package entities
 
+// import "go.mongodb.org/mongo-driver/bson"
+
 type Product struct {
-	Id       int64
-	Name     string
-	Price    float32
-	Quantity int
-	Status   bool
+	// Id       int64 "json"
+	// Name     string "json"
+	// Price    float32 "json"
+	// Quantity int "json"
+	// Status   bool "json"
 	// var id int64
 	// var name string
 	// var price float32
 	// var quantity int
 	// var status bool
+
+	ID        int    `json:"id"`
+	Nombre    string `json:"nombre"`
+	Apellidos string `json:"apellidos"`
+	Edad      int    `json:"edad"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
 }
